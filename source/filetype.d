ae2474b0348638e3aafa3c281fb2c33ec1fe6f07
Paul Crane <paul@acheronsoft.com>
2016-04-26 00:34:56 -0500

    Make use of raijin libs RecordCollector.

--- source/filetype.d
+++ source/filetype.d
@@ -6,8 +6,9 @@ import std.container : Array;
 import std.regex : Regex, ctRegex, matchFirst;
 import std.parallelism : parallel;
 
+import raijin.types.records;
+
 enum LanguageData = import("language.sdl");
-Regex!char _Pattern = ctRegex!(r"\s+(?P<key>\w+)\s+(?P<value>.*)");
 
 struct Record
 {
@@ -16,65 +17,12 @@ struct Record
 }
 
 alias RecordArray = Array!Record;
-alias StringArray = Array!string;
-
 RecordArray _DatArray;
 
 shared static this()
 {
-	loadFileTypeData();
-}
-
-private Record convertToRecord(StringArray strArray)
-{
-	Record data;
-
-	foreach(line; strArray)
-	{
-		auto re = matchFirst(line, _Pattern);
-
-		if(!re.empty)
-		{
-			immutable string key = re["key"].removechars("\"");
-			immutable string value = re["value"].removechars("\"");
-
-			final switch(key)
-			{
-				case "name":
-					data.name = value;
-					break;
-				case "extensions":
-					data.extensions = value;
-					break;
-			}
-		}
-	}
-
-	return data;
-}
-
-private void loadFileTypeData()
-{
-	import std.algorithm : canFind;
-	auto lines = LanguageData.lineSplitter();
-
-	StringArray strArray;
-
-	foreach(line; lines)
-	{
-		if(line.canFind("{"))
-		{
-			strArray.clear();
-		}
-		else if(line.canFind("}"))
-		{
-			_DatArray.insert(convertToRecord(strArray));
-		}
-		else
-		{
-			strArray.insert(line);
-		}
-	}
+	RecordCollector!Record collector;
+	_DatArray = collector.parse(LanguageData);
 }
 
 string getLanguageFromFileExtension(const string extension)
