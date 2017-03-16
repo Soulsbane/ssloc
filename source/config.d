50693f137313d5c7fea533fb5d95d19142d9a243
Paul Crane <paul@acheronsoft.com>
2017-03-16 04:07:38 -0500

    Add missing config module.

---
+++ source/config.d
@@ -0,0 +1,9 @@
+module config;
+
+public import ctoptions.getoptmixin;
+
+struct Options
+{
+	@GetOptOptions("Determines whether the results table is sorted by programming language.")
+	bool sort;
+}
