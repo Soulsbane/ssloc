703d80660abfc27f8769ba227a16b5ba1dd23b53
Paul Crane <paul@acheronsoft.com>
2017-10-01 01:26:45 -0500

    Make sorting results by programming language the default method.

--- source/config.d
+++ source/config.d
@@ -5,5 +5,5 @@ public import ctoptions.getoptmixin;
 struct Options
 {
 	@GetOptOptions("Determines whether the results table is sorted by programming language.")
-	bool sort;
+	bool sort = true;
 }
